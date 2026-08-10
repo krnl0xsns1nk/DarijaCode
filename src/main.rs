43670eea824c4921cca10cca0969d79ea07cd91c
krnl0xsns1nk <krnl0xsns1nk@gmail.com>
2026-08-10 12:43:11 +0100

    my really first rust code !

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+
+use std::env;
+mod hello;
+use hello::itIsWorking;
+
 fn main() {
     println!("wafin, al3alm!");
+    let ans: &str = "yes";
+    let mut args = env::args();
+    args.next().unwrap(); 
+    match args.next() {
+        Some(x) => {
+        match x.as_str() {
+            "khdm" => itIsWorking(&ans),
+            _ => println!("had l2amr mm3rofch: {}", x),
+        }
+        } 
+        None => println!("listi5dam: drj khdm <milf.drj>"),
+    }
 }
